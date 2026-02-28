65181885ea2c0b9f3f336284c41818a98c78daa4
butrintzeqiri6 <butrintzeqiri6@gmail.com>
2026-02-28 12:22:17 +0100

    weatherrr

---
+++ weatherapp/script.js
@@ -0,0 +1,37 @@
+const cityInput = document.getElementById('cityInput');
+const searchBtn = document.getElementById('searchBtn');
+const weatherResult = document.getElementById('weatherResult');
+const error = document.getElementById('error');
+const API_KEY = '2e30ef2483d10506ff30ae01eb9ae6ed';
+
+searchBtn.addEventListener('click', () => {
+    const city = cityInput.value.trim();
+    if (city === '') return;
+    getWeather(city);
+});
+
+cityInput.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') searchBtn.click()
+});
+
+async function getWeather(city) {
+    try {
+        error.textContent = '';
+        weatherResult.classList.add('d-none');
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
+        if(!response.ok) throw new Error('City not found');
+        const data = await response.json();
+        showWeather(data);
+    }
+    catch(err) {
+        error.textContent = err.message;
+    }
+}
+
+function showWeather(data) {
+    weatherResult.classList.remove('d-none');
+    weatherResult.innerHTML =`
+    <h4>${data.name}</h4>
+    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+    `;
+}

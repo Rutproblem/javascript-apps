e888e64694de488d30e702c5732a00e31ee5a916
butrintzeqiri6 <butrintzeqiri6@gmail.com>
2026-03-07 14:05:05 +0100

    f

--- quizzapp/main.js
+++ quizzapp/main.js
@@ -47,7 +47,79 @@ function showQuestion() {
     answersEl.innterHTML = "";
 
     q.answers.forEach((answer, index) => {
-        
-    })
+        const button = document.createElement('button');
+        button.className = "btn btn-outline-info";
+        button.innerText = answer;
 
+        button.addEventListener('click', () => {
+            checkAnswer(index);
+        })
+
+        answersEl.appendChild(button);
+    });
+
+    progressEl.innerText = `Question ${currentQuestion + 1} of ${questions.length}`;
 }
+
+function showAnimation(isCorrect) {
+    if(isCorrect) {
+        animationText.innerText = "Correct answer!";
+        animationText.className = "correctText!";
+        animationIcon.innerText = "✅";
+    }
+    else {
+        animationText.innerText = "Wrong answer!";
+        animationText.className = "wrongText!";
+        animationIcon.innerText = "❌";
+    }
+
+    animationOverlay.classList.add('show');
+
+    setTimeout(() => {
+        animationOverlay.classList.remove('show');
+    }, 3000);
+}
+
+function checkAnswer(selectedIndex) {
+    const correctIndex = questions[currentQuestion].correct;
+    const isCorrct = selectedIndex === correctIndex;
+
+    if(isCorrect) {
+        score++;
+    }
+
+    showAnimation(isCorrect);
+
+    setTimeout(() => {
+        currentQuestion++;
+
+        if(currentQuestion < questions.length) {
+            showQuestion();
+        }
+        else {
+            showResult();
+        }
+    },  3000);
+}
+
+function showResult() {
+    questionEl.innerText = `🎉Congrats, your score is: ${score} / ${questioins.length}🎉`;
+    answersEl.innerHTML = "";
+    progressEl.innerText = "";
+
+    const restartBtn = document.createElement('button');
+    restartBtn.className = "btn btn-success mt-3";
+    restartBtn.innerText = "Restart quiz";
+
+    restartBtn.addEventListener('click', restartQuiz);
+
+    answersEl.appendChild(restartBtn);
+}
+
+function restartQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    showQuestion();
+}
+
+showQuestion();

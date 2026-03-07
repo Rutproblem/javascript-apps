05cbb828a90ce15de7e52b4d3527e2456f79dcb0
butrintzeqiri6 <butrintzeqiri6@gmail.com>
2026-03-07 12:58:14 +0100

    question  setup

---
+++ quizzapp/main.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            "Hypertext Markup Language",
+            "High Text Machine Learning",
+            "Hyperlink Tool Markup  Language"
+        ],
+        correct: 0
+    },
+
+    {
+        question: "Which symbol is used for comments in Javacript?",
+        answers: [
+            "//",
+            "<!-- -->",
+            "**"
+        ],
+        correct: 0
+    },
+
+    {
+        question: "Which company created Javascript?",
+        answers: [
+            "Microsoft",
+            "Netscape",
+            "Google"
+        ],
+        correct: 1
+    },
+];
